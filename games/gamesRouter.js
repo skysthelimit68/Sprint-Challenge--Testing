09a4042be0605ba5ba0ee06520a54db7d6e556a9
Ming Liu <mliu@macpro.home>
2019-06-21 13:48:37 -0400

    routes tested, added additional dependencies

--- games/gamesRouter.js
+++ games/gamesRouter.js
@@ -1,17 +1,58 @@
-const router = require('express').Router;
+const router = require('express').Router();
 
 const Games = require('./gamesModel.js');
 
 router.get('/', (req, res) => {
     Games.getAll()
     .then( games => {
-        console.log(games)
         res.status(200).json(games)
     })
     .catch(error => {
         res.status(500).json(error)
     })
 })
+
+router.post('/', validateData, (req, res) => {
+    const game = req.body
+    Games.insert(game)
+    .then(res => {
+        res.status(201).json(res)
+    })
+    .catch(error=> {
+        res.status(500).json(error.message)
+    })
+})
+
+
+router.get('/:id', validateId, (req, res) => {
+    res.status(200).json(req.body.game)
+})
+
+//middleware
+
+function validateData(req, res, next) {
+    if(req.body && req.body.title && req.body.genre) next();
+    else {
+        res.status(422).json({message: "data provided is incomplete"})
+    }
+}
+
+function validateId(req, res, next) {
+    Games.findById(req.params.id)
+    .then( game => {
+        if(game) { 
+            req.body.game = game;
+            next();
+        }
+       
+        else {
+            res.status(404).json({message:"game not found"})
+        }
+    })
+    .catch( error => {  
+        res.status(500).json(error)
+    })
+}
 
 
 module.exports = router;
--- games/gamesRouter.test.js
+++ games/gamesRouter.test.js
@@ -1,5 +1,5 @@
 
-const router = require('./gamesRouter');
+const router = require('../api/server');
 const supertest = require('supertest');
 const db = require('../data/dbConfig.js');
 
@@ -8,10 +8,43 @@ describe('router', () => {
         await db('users').truncate();
     })
 
-    it('respond with 200 - get - all games', () => {
-        return supertest(router)
-        .get('/')
-        .expect(200)
+    it('respond with 200 - get - all games', async () => {
+        const response = await supertest(router).get('/')
+        expect(response.status).toEqual(200)
+    })
+
+    it('respond with a json object - get - all games', async () => {
+        await supertest(router)
+        .get('/api/games')
         .expect('Content-Type', /json/i)
     })
+
+    it('respond with an array - get - all games', async() => {
+        const response = await supertest(router)
+        .get('/api/games');
+        console.log(response)
+        expect(Array.isArray(response.body)).toBe(true);
+    })
+
+    it('respond with 201 - post - add game', async () => {
+        const response = await supertest(router)
+        .post('/api/games')
+        .send({title: 'testgame333', genre: 'testgenr3e'})
+        console.log(response.body)
+        expect(response.status).toBe(201)
+    })
+
+    it('respond with json object - post - add game', async () => {
+         await supertest(router)
+            .post('/api/games')
+            .send({title: 'testgame2', genre: 'testgenre'})
+            .expect('Content-Type', /json/i)
+    })    
+    it('should respond with 422 - post - add game w/ incomplete data', async () => {
+        await supertest(router)
+            .post('/api/games')
+            .send({title: 'testgame123'})
+            .expect(422)
+        })
 })
+
